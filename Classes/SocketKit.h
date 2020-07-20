6fb65d4f0cbff81d1872b77697f013c5fd3395b6
rhc <chenhairong@itouchtv.cn>
2020-07-20 14:02:24 +0800

    file

---
+++ Classes/SocketKit.h
@@ -0,0 +1,17 @@
+//
+//  SocketKit.h
+//  TouchTV
+//
+//  Created by William on 2020/7/20.
+//  Copyright © 2020 TouchTV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SocketKit : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/SocketKit.m
@@ -0,0 +1,13 @@
+//
+//  SocketKit.m
+//  TouchTV
+//
+//  Created by William on 2020/7/20.
+//  Copyright © 2020 TouchTV. All rights reserved.
+//
+
+#import "SocketKit.h"
+
+@implementation SocketKit
+
+@end
